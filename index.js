d5a256113356164523ec80d2f19978bb57eece05
Thomás Matos <165833974+ThomasCapim@users.noreply.github.com>
2024-11-21 17:17:38 +0000

    t

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-import express from "express";      // Requisição do pacote do express
-
+//index.js
 import dotenv from "dotenv";
-dotenv.config();
-
+import express from "express";
 import { selectUsuarios } from "./bd.js";
 
-const app = express();              // Instancia o Express
-const port = 3000;                  // Define a porta
+dotenv.config();
 
-app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
-    console.log("Rota GET/ solicitada");
-    res.json({
-        nome: "Thomás Matos Mendes",      // Substitua pelo seu nome
-    });
+const app = express(); // Instancia o Express
+const port = 3000; // Define a porta
+
+app.get("/", (req, res) => {
+  console.log("Rota / solicitada");
+  // Cria a rota da raiz do projeto
+  res.json({
+    nome: "Thomás Matos Mendes", // Substitua pelo seu nome
+  });
 });
 
-app.listen(port, () => {            // Um socket para "escutar" as requisições
-    console.log(`Serviço escutando na porta:  ${port}`);
+app.get("/usuarios", async (req, res) => {
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+
+  console.log("Rota GET/usuarios solicitada");
+});
+
+app.listen(port, () => {
+  // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
 });
