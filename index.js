bf5a2cef0436d7febe1358b197a49d2753d81c0c
ThomasCapim <tmm9@aluno.ifnmg.edu.br>
2024-11-21 13:19:54 -0300

    t

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import express from "express";      // Requisição do pacote do express
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import { selectUsuarios } from "./bd.js";
+
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
