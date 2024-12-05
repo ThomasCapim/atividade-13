3cb1a6f69e9672731d44a5aa7b3c425cf58e7108
Thomás Matos <165833974+ThomasCapim@users.noreply.github.com>
2024-12-05 17:42:19 +0000

    t

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ dotenv.config();
 const app = express(); // Instancia o Express
 const port = 3000; // Define a porta
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   console.log("Rota / solicitada");
   // Cria a rota da raiz do projeto
@@ -22,21 +24,24 @@ app.get("/usuarios", async (req, res) => {
     res.json(usuarios);
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
-}
-
-  app.get("/usuario/:id", async (req, res) => {
-    console.log("Rota GET /usuario/# solicitada");
-    try {
-      const usuario = await selectUsuario(req.params.id);
-      if (usuario.length > 0) res.json(usuario);
-      else res.status(404).json({ message: "Usuário não encontrado!" });
-    } catch (error) {
-      res.status(error.status || 500).json({ message: error.message || "Erro!" });
-    }
-  });
-
+  }
   console.log("Rota GET/usuarios solicitada");
 });
+
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario/# solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+
+
+
 
 app.listen(port, () => {
   // Um socket para "escutar" as requisições
